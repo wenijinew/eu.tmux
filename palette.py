079de25ce059ecf21a2814e61847e1e871a5694a
Bruce Wen <wenijinew@gmail.com>
2023-09-04 16:12:06 +0200

    Code refactoring

--- palette.py
+++ palette.py
@@ -68,56 +68,68 @@ def generate_random_colors(
     return dark_colors
 
 
-def generate_random_dark_red(n_colors=1):
+def generate_random_red(n_colors=1, lightness=15):
     """Generate random dark red colors"""
+    lightness_min = lightness
+    lightness_max = lightness_min + 25
     hue = random.randint(300, 360) / 360
     saturation = random.randint(75, 100) / 100
-    lightness = random.randint(15, 40) / 100
+    lightness = random.randint(lightness_min, lightness_max) / 100
 
     return generate_random_colors(n_colors, hue, saturation, lightness)
 
 
-def generate_random_dark_orange(n_colors=1):
+def generate_random_orange(n_colors=1, lightness=20):
     """Generate random dark orange colors"""
+    lightness_min = lightness
+    lightness_max = lightness_min + 20
     hue = random.randint(20, 60) / 360
     saturation = random.randint(60, 100) / 100
-    lightness = random.randint(20, 40) / 100
+    lightness = random.randint(lightness_min, lightness_max) / 100
 
     return generate_random_colors(n_colors, hue, saturation, lightness)
 
 
-def generate_random_dark_purple(n_colors=1):
+def generate_random_purple(n_colors=1, lightness=20):
     """Generate random dark orange colors"""
+    lightness_min = lightness
+    lightness_max = lightness_min + 20
     hue = random.randint(270, 330) / 360
     saturation = random.randint(60, 100) / 100
-    lightness = random.randint(20, 40) / 100
+    lightness = random.randint(lightness_min, lightness_max) / 100
 
     return generate_random_colors(n_colors, hue, saturation, lightness)
 
 
-def generate_random_dark_black(n_colors=1):
+def generate_random_black(n_colors=1, lightness=0):
     """Generate random dark black/gray/blue colors"""
+    lightness_min = lightness
+    lightness_max = lightness_min + 10
     hue = random.randint(0, 360) / 360
     saturation = 1
-    lightness = random.randint(0, 10) / 100
+    lightness = random.randint(lightness_min, lightness_max) / 100
 
     return generate_random_colors(n_colors, hue, saturation, lightness)
 
 
-def generate_random_dark_blue(n_colors=1):
+def generate_random_blue(n_colors=1, lightness=0):
     """Generate random dark black/gray/blue colors"""
+    lightness_min = lightness
+    lightness_max = lightness_min + 15
     hue = random.randint(180, 240) / 360
     saturation = random.randint(75, 100) / 100
-    lightness = random.randint(0, 15) / 100
+    lightness = random.randint(lightness_min, lightness_max) / 100
 
     return generate_random_colors(n_colors, hue, saturation, lightness)
 
 
-def generate_random_dark_green(n_colors=1):
+def generate_random_green(n_colors=1, lightness=10):
     """Generate random dark green colors"""
+    lightness_min = lightness
+    lightness_max = lightness_min + 10
     hue = random.randint(90, 150) / 360
     saturation = random.randint(60, 100) / 100
-    lightness = random.randint(10, 20) / 100
+    lightness = random.randint(lightness_min, lightness_max) / 100
 
     return generate_random_colors(n_colors, hue, saturation, lightness)
 
@@ -132,21 +144,21 @@ def generate_palette(n_palette=1):
     for _i in range(n_palette):
         palette = []
 
-        dark_red = generate_random_dark_red()
-        dark_purple = generate_random_dark_purple()
-        dark_orange = generate_random_dark_orange()
-        dark_green = generate_random_dark_green()
-        dark_blue = generate_random_dark_blue()
-        dark_black = generate_random_dark_black()
+        red = generate_random_red()
+        purple = generate_random_purple()
+        orange = generate_random_orange()
+        green = generate_random_green()
+        blue = generate_random_blue()
+        black = generate_random_black()
         for dark_color in [
             c
             for dark in (
-                dark_red,
-                dark_purple,
-                dark_orange,
-                dark_green,
-                dark_blue,
-                dark_black,
+                red,
+                purple,
+                orange,
+                green,
+                blue,
+                black,
             )
             for c in dark
         ]:
